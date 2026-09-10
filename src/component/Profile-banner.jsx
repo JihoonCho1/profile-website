bc601c5756da36d5c588abff86bb7e3cab9d10f8
JihoonCho1 <choij2821@gmail.com>
2026-09-11 03:51:10 +1200

    Fix: profile banner min width applided

--- src/component/Profile-banner.jsx
+++ src/component/Profile-banner.jsx
@@ -2,16 +2,20 @@ import './profile-banner.css'
 import resume from '../assets/resume.png'
 import githubIcon from '../assets/github-icon.png'
 import coffeeBar from '../assets/caffeine-bar.png'
+import profile from '../assets/dav_pfp.png'
 
 const ProfileBanner = () => {
     return (
         <div className="banner-wrapper">
+            <div className="profile-picture">
+                <img src={profile} className="pfp"/>
+            </div>
             <div className="text-field">
                 <p>Name: Jihoon Choi</p>
                 <p>Role: Frontend/Backend</p>
                 <div className="link-wrapper">
                 <p>Resume:{' '}
-                    <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+                    <a href="/Resume.pdf" target="_blank" rel="noopener noreferrer">
                         <img 
                         src={resume}
                         alt="resume"
